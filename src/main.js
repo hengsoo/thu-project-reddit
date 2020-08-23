7a8d8d37f9693bb2d77a7a21b9691329627a4900
hengsoo <hengsoo.sjh@gmail.com>
2020-08-23 16:21:05 +0800

    Make axios global

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@/assets/css/tailwind.css'
 import VueCookie from 'vue-cookie'
+import Axios from 'axios'
+
+Vue.prototype.$http = Axios;
 
 Vue.config.productionTip = false
 
