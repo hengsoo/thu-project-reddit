15cde6d3ed401aaa6e908d55866e2852d3a833a4
hengsoo <hengsoo.sjh@gmail.com>
2020-08-23 16:17:49 +0800

    Add vue-cookie

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/css/tailwind.css'
+import VueCookie from 'vue-cookie'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCookie);
 
 new Vue({
   router,
