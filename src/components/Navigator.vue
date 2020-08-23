15e787cfc3f63c8da14b4466420f97b1616b3654
hengsoo <hengsoo.sjh@gmail.com>
2020-08-24 01:43:42 +0800

    Add padding

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,7 +1,7 @@
 <template>
 
   <nav class="bg-teal-500 px-3 pt-3 pb-2">
-    <div class="container mx-auto flex items-baseline justify-between flex-wrap ">
+    <div class="container mx-auto sm:px-10 flex items-baseline justify-between flex-wrap ">
       <div class="flex items-center flex-shrink-0 text-white mr-6 ml-3">
         <span class="font-semibold text-3xl tracking-tight">Tokok</span>
       </div>
